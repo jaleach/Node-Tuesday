11e86358d54b222ec294b100f99956726ba25bea
jim leach <yogajim@gmail.com>
2015-07-14 11:40:58 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+  app = express();
+
+app.set('view engine', 'jade');
+
+app.use( express.static( process.cwd() + '/public'));
+
+app.get('/', function(request, response){
+
+  response.render('index', {
+    myCoolMessage: 'Jade rocks!!!'
+
+  });
+
+});
+app.get('/greet/:name', function(req, res){
+  var names = ['jd', 'tim', 'spencer'];
+
+  var name = req.params.name;
+
+  if (names.indexOf(name) >= 0) {
+    res.render('greet', {
+    name: req.params.name
+  });
+  }
+  else {
+
+  res.status(404).render('404');
+  }
+
+});
+console.log('listening on locahost:8025');
+app.listen(8025);
